82e31bfdc111d719126d35dd800c6c2f9c852d3a
masterUNG <phrombutr@gmail.com>
2019-04-03 07:45:18 +0700

    update

--- app/src/main/java/masterung/th/in/androidthai/mariealert/AuthenFragment.java
+++ app/src/main/java/masterung/th/in/androidthai/mariealert/AuthenFragment.java
@@ -1,11 +1,15 @@
 package masterung.th.in.androidthai.mariealert;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 
 /**
@@ -13,11 +17,45 @@ import android.view.ViewGroup;
  */
 public class AuthenFragment extends Fragment {
 
+    private CheckBox checkBox;
+    private boolean statusLoginABoolean = false;
+    private MyConstant myConstant = new MyConstant();
+
 
     public AuthenFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//       Initial View
+        checkBox = getView().findViewById(R.id.chbRemember);
+
+
+//        Check Remember
+        checkRemember();
+
+    }   // Main Method
+
+    private void checkRemember() {
+
+        try {
+
+            SharedPreferences sharedPreferences = getActivity()
+                    .getSharedPreferences(myConstant.getRememberFile(), Context.MODE_PRIVATE);
+            statusLoginABoolean = sharedPreferences.getBoolean("StatusLogin", false);
+
+            if (statusLoginABoolean) {
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
---
+++ app/src/main/java/masterung/th/in/androidthai/mariealert/MyConstant.java
@@ -0,0 +1,15 @@
+package masterung.th.in.androidthai.mariealert;
+
+public class MyConstant {
+
+    //    Field
+    private String rememberFile = "remember";
+
+
+//    Getter
+
+
+    public String getRememberFile() {
+        return rememberFile;
+    }
+}
