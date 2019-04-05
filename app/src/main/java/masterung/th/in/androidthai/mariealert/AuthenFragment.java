1f7d73340db9f3ba4f576de7a593d7f1db914dcb
masterUNG <phrombutr@gmail.com>
2019-04-05 09:31:34 +0700

    update

--- app/src/main/java/masterung/th/in/androidthai/mariealert/AuthenFragment.java
+++ app/src/main/java/masterung/th/in/androidthai/mariealert/AuthenFragment.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -61,6 +62,25 @@ public class AuthenFragment extends Fragment {
 //                    Have Space
                     masterAlert.normalDialog(myConstant.getTitleHaveSpace(), myConstant.getMessageHaveSpace());
                 } else {
+
+                    try {
+
+                        GetUserWhereOneColumnThread getUserWhereOneColumnThread = new GetUserWhereOneColumnThread(getActivity());
+                        getUserWhereOneColumnThread.execute("User", user, myConstant.getUrlGetUserWhereUser());
+                        String result = getUserWhereOneColumnThread.get();
+                        Log.d("5AprilV1", "result ==>>> " + result);
+
+                        if (result.equals("null")) {
+                            masterAlert.normalDialog(myConstant.getTitleUserFalse(), myConstant.getMessageUserFalse());
+                        } else {
+
+                        }
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
                 }
 
             }   // onClick
--- app/src/main/java/masterung/th/in/androidthai/mariealert/MyConstant.java
+++ app/src/main/java/masterung/th/in/androidthai/mariealert/MyConstant.java
@@ -8,12 +8,32 @@ public class MyConstant {
     private String rememberFile = "remember";
     private String titleHaveSpace = "มีช่องว่างเกิิดขึ้น";
     private String messageHaveSpace = "กรุณากรอง ทุกช่อง คะ";
+    private String titleUserFalse = "User ผิด";
+    private String messageUserFalse = "ไม่มี User นี่ในฐานข้อมูล คะ";
+    private String titlePasswordFalse = "Password ผิด";
+    private String messagePasswordFalse = "พิมพ์ Password ใหม่ คะ";
 
 
 
 
 //    Getter
 
+
+    public String getTitleUserFalse() {
+        return titleUserFalse;
+    }
+
+    public String getMessageUserFalse() {
+        return messageUserFalse;
+    }
+
+    public String getTitlePasswordFalse() {
+        return titlePasswordFalse;
+    }
+
+    public String getMessagePasswordFalse() {
+        return messagePasswordFalse;
+    }
 
     public String getUrlGetUserWhereUser() {
         return urlGetUserWhereUser;
