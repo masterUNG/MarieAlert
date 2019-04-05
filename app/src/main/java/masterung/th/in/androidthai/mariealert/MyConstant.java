ad0e297ecffd64ec7370643fce1442f698d2b94e
masterUNG <phrombutr@gmail.com>
2019-04-05 08:14:17 +0700

    update

--- app/src/main/java/masterung/th/in/androidthai/mariealert/MyConstant.java
+++ app/src/main/java/masterung/th/in/androidthai/mariealert/MyConstant.java
@@ -3,13 +3,21 @@ package masterung.th.in.androidthai.mariealert;
 public class MyConstant {
 
     //    Field
+    private String urlGetUserWhereUser = "http://tscore.ms.ac.th/App/getUserWhereUserAndroid.php";
+
     private String rememberFile = "remember";
     private String titleHaveSpace = "มีช่องว่างเกิิดขึ้น";
     private String messageHaveSpace = "กรุณากรอง ทุกช่อง คะ";
 
 
+
+
 //    Getter
 
+
+    public String getUrlGetUserWhereUser() {
+        return urlGetUserWhereUser;
+    }
 
     public String getTitleHaveSpace() {
         return titleHaveSpace;
